dbebf73d1a87badb4eb1f18b8e088f8ccd7d06d5
Ambar Agrawal <amb.ar4eve@gmail.com>
2017-05-22 14:06:13 +0545

    generate Notes controller and add resource routes

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,35 @@
+class NotesController < ApplicationController
+    def index
+
+    end
+
+    def show
+
+    end
+    
+    def create
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destory
+
+    end
+
+    private
+
+    def find_note
+
+    end
+
+    def note_params
+
+    end
+end
