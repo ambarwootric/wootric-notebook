252a1e5ad10461939b9f95c800f855ffed565b38
Ambar Agrawal <amb.ar4eve@gmail.com>
2017-05-22 19:38:33 +0545

    installing devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ WootricNotebook::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Needed for devise gem
+   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
